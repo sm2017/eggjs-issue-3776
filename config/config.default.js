28dd4451b28d962bb99ab39b7e039a0a9fba3417
SM2017 <socketman2016@gmail.com>
2019-07-01 09:40:47 +0430

    Added domainWhiteList

--- config/config.default.js
+++ config/config.default.js
@@ -18,6 +18,14 @@ module.exports = appInfo => {
   // add your middleware config here
   config.middleware = [];
 
+  config.security = {
+    domainWhiteList: [
+      'http://localhost:7001',
+      'http://localhost:7002',
+      'http://localhost:7003',
+    ],
+  };
+
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
